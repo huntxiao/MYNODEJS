d03f5e838aebd8ba66ed4c2a35ea8d518b03c998
huntxiao <huntxiao@gmail.com>
2019-07-02 01:24:31 +0800

    express

---
+++ my-express/http.md
@@ -0,0 +1,11 @@
+#http methods--RESTful Convention
+
+GET /api/customers
+
+GET /api/customers/1
+
+PUT /api/customers/1
+
+DELETE /api/customers/1
+
+POST /api/customers
---
+++ my-express/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+// app.post();
+// app.put();
+// app.delete();
+
+//manage the PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
