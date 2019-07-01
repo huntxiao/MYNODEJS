f2490debeb304d2778debf931180eedf151ab722
huntxiao <huntxiao@gmail.com>
2019-07-01 23:53:01 +0800

    extends eventEmitter

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const logger = require("./logger");
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
 const EventEmitter = require("events");
 
-const emitter = new EventEmitter();
+// const emitter = new EventEmitter(); //通过类实现
 
-emitter.console.log(filesSync);
-var filesSync = fs.readdirSync("./");
-fs.readdir("$", (err, files) => {
-  if (err) console.log("Error", err);
-  else console.log("Result", files);
+const Logger = require("./logger");
+const logger = new Logger();
+
+//Register a listener
+logger.on("messageLogged", arg => {
+  console.log("Listener Called", arg);
 });
-// console.log(filesdir);
+logger.log("message");
+
+// var filesSync = fs.readdirSync("./");
+
+// fs.readdir("$", (err, files) => {
+//   if (err) console.log("Error", err);
+//   //异步方法
+//   else console.log("Result", files);
+// });
 
 //node has no window object
--- logger.js
+++ logger.js
@@ -1,9 +1,17 @@
+const EventEmitter = require("events");
+// const emitter = new EventEmitter(); //通过类实现
+
 let url = "http://mylogger.io/log";
 
-log = message => {
-  //send Http request;
-  console.log(message);
-};
+class Logger extends EventEmitter {
+  log(message) {
+    //send Http request;
+    console.log(message);
+    //Raise an Event ,Making a noise or Produce sth
+    this.emit("messageLogged", { id: 1, url: "http://" });
+  }
+}
 
-module.exports.log = log;
+// module.exports.log = log;
+module.exports = Logger;
 module.exports.endPoint = url;
