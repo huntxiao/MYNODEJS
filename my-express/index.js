af81e86852d0b791a479f9db0022e01c42933800
huntxiao <huntxiao@gmail.com>
2019-07-03 00:26:52 +0800

    restful and joi validation middleware

--- my-express/index.js
+++ my-express/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const Joi = require("joi"); //返回值为class
 
 const app = express();
 
 app.use(express.json());
+
+//Midlleware
+
+app.use(function(req, res, next) {
+  console.log("logging...");
+  next(); //Control rights transfer to next Middleware
+});
+
+app.use(function(req, res, next) {
+  console.log("Athenticating...");
+  next(); //Control rights transfer to next Middleware
+});
 
 const courses = [
   { id: 1, name: "courses1" },
@@ -29,10 +42,18 @@ app.get("/api/posts/:year/:month", (req, res) => {
 
 //POST
 app.post("/api/courses", (req, res) => {
-  if (!req.body.name || req.body.name.length < 3) {
-    res.status(404).send("name is required and Name Should be minimum 3");
+  //可以使用npm-joi来进行验证
+  const { error } = validateCourse(req.body);
+  console.log(error); //result.error
+
+  if (error) {
+    res.status(400).send(error);
     return;
   }
+  // if (!req.body.name || req.body.name.length < 3) {
+  //   res.status(404).send("name is required and Name Should be minimum 3");
+  //   return;
+  // }
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -40,6 +61,30 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+//PUT
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course is not found");
+  } else {
+    res.send(course);
+  }
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error);
+    return;
+  }
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  const result = Joi.validate(course, schema);
+}
 
 //manage the PORT
 const port = process.env.PORT || 3000;
