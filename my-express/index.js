f0136024be1a6d3aa6a1e0d784f35ed59f0cfdcf
huntxiao <huntxiao@gmail.com>
2019-07-02 23:40:02 +0800

    post

--- my-express/index.js
+++ my-express/index.js
@@ -2,16 +2,44 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "courses1" },
+  { id: 2, name: "courses2" },
+  { id: 3, name: "courses3" }
+];
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
-// app.post();
-// app.put();
-// app.delete();
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course is not found");
+  } else {
+    res.send(course);
+  }
+});
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+//POST
+app.post("/api/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    res.status(404).send("name is required and Name Should be minimum 3");
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
 
 //manage the PORT
 const port = process.env.PORT || 3000;
